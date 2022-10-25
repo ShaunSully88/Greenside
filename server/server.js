bce67c6c38b584525c020c09ae145be9847d0fce
Shaun Sullivan <shaunsully88@gmail.com>
2022-10-25 11:24:23 -0400

    server.js file set up, PORT set up, express and Apollo express set up

---
+++ server/server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const {ApolloServer} = require('apollo-server-express');
+const path = require('path');
+const {typeDefs, resolvers} = require('./schemas');
+const {authMiddleware} = require('./utils/auth');
+const db = require('./config/connection');
+const PORT = process.env.PORT || 3001;
+const server = newApolloServer({
+    typeDefs,
+    resolvers,
+    context: authMiddleware,
+});
+
+const app = express();
+app.use(express.urlencoded({ etended: false }));
+app.use(express.json());
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, //needs to be filled))
+})
+
+const startApolloServer = async (typeDefs, resolvers) => {
+    await server.start();
+    server.applyMiddleware({ app });
+    db.once('open', () => {
+        app.listen(PORT, ()=> {
+            console.log(`API server running on port ${PORT}!`);
+        })
+    })
+}
+
+startApolloServer(typeDefs, resolvers);
