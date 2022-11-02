514636da9e9eece661b9e256a9bb316881c17a7d
Shaun Sullivan <shaunsully88@gmail.com>
2022-11-02 13:56:28 -0400

    central courses added to seeds

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -164,7 +164,172 @@ db.once('open', async () => {
             image:   
         },
         {
-            
+            name: 'Irish Hills Golf Club',
+            location: location[0]._id,
+            par: 71,
+            holes: 27,
+            price: 39.00,
+            image:
+        },
+        {
+            name: 'Casselview Golf and Country Club',
+            location: location[0]._id,
+            par: 72,
+            holes: 18,
+            price: 50.00,
+            image: 
+        },
+        {
+            name: 'Larchmere Golf Club',
+            location: location[0]._id,
+            par: 33,
+            holes: 9,
+            price: 30,
+            image: 
+        },
+        {
+            name: 'Byrnell Golf Club',
+            location: location[1]._id,
+            par: 30,
+            holes: 9,
+            price: 35.00,
+            image: 
+        },
+        {
+            name: 'Barry\'s Bay Golf Club',
+            location: location[1]._id,
+            par: 36,
+            holes: 9,
+            price: 35.00,
+            image: 
+        },
+        {
+            name: 'Duntroon Highlands',
+            location: location[1]._id,
+            par: 72,
+            holes: 18,
+            price: 60.00,
+            image: 
+        },
+        {
+            name: 'The Briars Golf Club',
+            location: location[1]._id,
+            par: 72,
+            holes: 18,
+            price: 75.00,
+            image: 
+        },
+        {
+            name: 'Monterra Golf',
+            location: location[1]._id,
+            par: 72,
+            holes: 18,
+            price: 80.00,
+            image: 
+        },
+        {
+            name: 'Batteaux Creek Golf Club',
+            location: location[1]._id,
+            par: 72,
+            holes: 18,
+            price: 70.00,
+            image: 
+        },
+        {
+            name: 'Tangle Creek',
+            location: location[1]._id,
+            par: 72,
+            holes: 18,
+            price: 70.00,
+            image:
+        },
+        {
+            name: 'Settlers\' Ghost Golf Club',
+            location: location[1]._id,
+            par: 72,
+            holes: 18,
+            price: 60.00,
+            image: 
+        },
+        {
+            name: 'Black Bear Ridge',
+            location: location[1]._id,
+            par: 72,
+            holes: 27,
+            price: 65.00,
+            image: 
+        },
+        {
+            name: 'Hockley Valley Resort',
+            location: location[1]._id,
+            par: 71,
+            holes: 18,
+            price: 100.00,
+            image: 
+        },
+        {
+            name: 'Timber Ridge Golf Course',
+            location: location[1]._id,
+            par: 72,
+            holes: 18,
+            price: 55.00,
+            image: 
+        },
+        {
+            name: 'Innisbrook',
+            location: location[1]._id,
+            par: 64,
+            holes: 18,
+            price: 50.00,
+            image: 
+        },
+        {
+            name: 'Brooklea Golf Club',
+            location: location[1]._id,
+            par: 72,
+            holes: 27,
+            price: 50.00,
+            image:
+        },
+        {
+            name: 'Roundel Glen Golf Course',
+            location: location[1]._id,
+            par: 72,
+            holes: 18,
+            price: 45.00,
+            image: 
+        },
+        {
+            name: 'Woodington Lake Golf Club',
+            location: location[1]._id,
+            par: 72,
+            holes: 36,
+            price: 95.00,
+            image:
+        },
+        {
+            name: 'Braestone',
+            location: location[1]._id,
+            par: 71,
+            holes: 18,
+            price: 95.00,
+            image:
+        },
+        {
+            name: 'Nottawasaga Golf Course',
+            location: location[1]._id,
+            par: 72,
+            holes: 27,
+            price: 75.00,
+            image: 
+        },
+        {
+            name: 'The Club at Bond Head',
+            location: location[2]._id,
+            par: 72,
+            holes: 36,
+            price: 100.00,
+            image:
         }
     ])
 })
